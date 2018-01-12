dda6b7d25be90736e4eff78e197c6dd2e52cae7d
Danverde <gre12014@byui.edu>
2018-01-12 11:36:52 -0700

    now enables and sets locking by object type

--- main.js
+++ main.js
@@ -6,7 +6,8 @@
 /* Makes the current course a blueprint course*/
 
 /* Put dependencies here */
-const canvas = require('canvas-wrapper');
+const canvas = require('canvas-wrapper'),
+    asyncLib = require('async');
 
 module.exports = (course, stepCallback) => {
     course.addModuleReport('course-make-blueprint');
@@ -19,7 +20,10 @@ module.exports = (course, stepCallback) => {
         return;
     }
 
-    // make the course a blueprint
+    /*********************************
+     * START HERE
+     * make the course a blueprint 
+     **********************************/
     canvas.put(`/api/v1/courses/${course.info.canvasOU}`, {
         'course[blueprint]': true
     }, (err, res) => {
@@ -29,7 +33,39 @@ module.exports = (course, stepCallback) => {
             return;
         }
         course.info.isBlueprint = true;
-        course.success('course-make-blueprint', `${course.info.courseName} is now a blueprint course`);
-        stepCallback(null, course);
+        course.success('course-make-blueprint', `${course.info.fileName.split('.zip')[0]} is now a blueprint course`);
+
+        /* Enable locking items by object */
+        canvas.put(`/api/v1/courses/${course.info.canvasOU}`, {
+            'course[use_blueprint_restrictions_by_object_type]': true,
+        }, (err) => {
+            if (err) {
+                course.throwError('blueprint-lock-items', err);
+                stepCallback(null, err);
+                return;
+            }
+            course.success('course-make-blueprint', 'Locking items by object type enabled');
+
+            /* Enable locking points & content on all obj types */
+            resObj = {
+                'course[blueprint_restrictions_by_object_type]': {
+                    'assignment': {'content': true, 'points': true},
+                    'attachment': {'content': true},
+                    'discussion_topic': {'content': true, 'points': true},
+                    'quiz': {'content': true, 'points': true},
+                    'wiki_page': {'content': true}
+                }
+            }
+
+            canvas.put(`/api/v1/courses/${course.info.canvasOU}`, resObj, (err, result) => {
+                if (err) {
+                    course.throwErr('course-make-blueprint', err);
+                    stepCallback(null, course);
+                    return;
+                }
+                course.success('course-make-blueprint', 'Content and points locked for all object types');
+                stepCallback(null, course);
+            });
+        });
     });
 };
