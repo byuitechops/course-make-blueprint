b9309fc8aee8f9626fcfcbdd4530f45ab7766388
Zachary Williams <wil11063@byui.edu>
2018-04-24 09:23:53 -0600

    Add missing return statement

--- Tests/childTests.js
+++ Tests/childTests.js
@@ -3,12 +3,13 @@ const tap = require('tap');
 const canvas = require('canvas-wrapper');
 
 module.exports = (course, callback) => {
-    tap.test('child-template', (test) => {
+    tap.test('course-make-blueprint', (test) => {
 
         canvas.get(`/api/v1/courses/${course.info.canvasOU}`, (err, courseArr) => {
             if (err) {
                 course.error(err);
                 test.end();
+                return;
             }
 
             test.ok(courseArr[0].blueprint, true);
